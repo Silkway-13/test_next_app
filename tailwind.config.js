0495e96bf68067bc76a661b222f46d3358b79f93
bayr.khan <bayrsaikhan.ger13@gmail.com>
2023-08-14 18:06:19 +0800

    all done.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
